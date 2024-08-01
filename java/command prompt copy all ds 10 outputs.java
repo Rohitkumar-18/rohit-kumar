b531a8b87d4043bf9d2267a30f4d8673e328cbb7
Rohitkumar-18 <155879840+Rohitkumar-18@users.noreply.github.com>
2024-08-01 15:41:13 +0530

    Add files via upload

---
+++ java/command prompt copy all ds 10 outputs.java
@@ -0,0 +1,646 @@
+Microsoft Windows [Version 10.0.19045.4529]
+(c) Microsoft Corporation. All rights reserved.
+
+M:\>cd mca
+M:\mca>cd ds
+
+M:\mca\DS>javac ArrayOperationImplementation.java
+
+M:\mca\DS>java ArrayOperationImplementation
+*** MENU for array operation***
+
+         1.insertion at last
+         2.insert element at middle
+        3.delete at last
+         4.delete at middle
+         5.sorting
+         6.searching
+         7.traversing
+Enter your choice
+1
+please enter the element to be inserted
+2
+
+         do you want to continue
+y
+*** MENU for array operation***
+
+         1.insertion at last
+         2.insert element at middle
+        3.delete at last
+         4.delete at middle
+         5.sorting
+         6.searching
+         7.traversing
+Enter your choice
+2
+please enter the element to be inserted
+4
+please enter the position where element to be inserted
+2
+
+         do you want to continue
+y
+*** MENU for array operation***
+
+         1.insertion at last
+         2.insert element at middle
+        3.delete at last
+         4.delete at middle
+         5.sorting
+         6.searching
+         7.traversing
+Enter your choice
+5
+
+         elemnts in the array are sorted,please select traverse to see the sorted elements::
+
+         do you want to continue
+y
+*** MENU for array operation***
+
+         1.insertion at last
+         2.insert element at middle
+        3.delete at last
+         4.delete at middle
+         5.sorting
+         6.searching
+         7.traversing
+Enter your choice
+6
+enter the element to be search
+3
+
+         search is un successfull
+
+         element is not there in an array
+
+         do you want to continue
+y
+*** MENU for array operation***
+
+         1.insertion at last
+         2.insert element at middle
+        3.delete at last
+         4.delete at middle
+         5.sorting
+         6.searching
+         7.traversing
+Enter your choice
+7
+
+         the elements in the array are:
+4
+2
+
+         do you want to continue
+n
+Thank you and my work is over
+
+M:\mca\DS>javac SingleLinkedList.java
+
+M:\mca\DS>java SingleLinkedList
+........Menu........
+
+                 1. Add Node At Front
+                 2. Add Node At Rear
+                 3. Remove Node At Front
+                 4. Remove At Last
+                 5.Add Node At Position
+                 6. Remove Node At Any pos
+                 7. Display elements in list
+
+
+
+ Enter your choice
+
+1
+Enter data part to a node
+2
+
+
+ Do you want to contine....
+ Press y or Y for(Yes)|| n or N for(No)
+y
+........Menu........
+
+                 1. Add Node At Front
+                 2. Add Node At Rear
+                 3. Remove Node At Front
+                 4. Remove At Last
+                 5.Add Node At Position
+                 6. Remove Node At Any pos
+                 7. Display elements in list
+
+
+
+ Enter your choice
+
+2
+Enter data part to a node
+5
+
+
+ Do you want to contine....
+ Press y or Y for(Yes)|| n or N for(No)
+5
+........Menu........
+
+                 1. Add Node At Front
+                 2. Add Node At Rear
+                 3. Remove Node At Front
+                 4. Remove At Last
+                 5.Add Node At Position
+                 6. Remove Node At Any pos
+                 7. Display elements in list
+
+
+
+ Enter your choice
+
+7
+2-->5-->null
+
+
+ Do you want to contine....
+ Press y or Y for(Yes)|| n or N for(No)
+n
+
+
+ Thank you see you again..
+
+
+
+M:\mca\DS>javac StackLinked.java
+
+M:\mca\DS>java StackLinked
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+1
+enter the insert node
+2
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+2
+the pop of the ele2
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+
+3
+Stack is empty
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+1
+enter the insert node
+5
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+
+3
+the top of the stack5
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+4
+5-->
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+5
+
+M:\mca\DS>javac StackLink.java
+
+M:\mca\DS>java StackLinked
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+1
+enter the insert node
+2
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+2
+the pop of the ele2
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+3
+Stack is empty
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+1
+enter the insert node
+6
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+2
+the pop of the ele6
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+7
+wrong choice
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+3
+Stack is empty
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+4
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+4
+enter ur choice
+ 1.push
+ 2.pop
+ 3.peek
+ 4.display
+ 5.exit
+5
+
+M:\mca\DS>javac DLink.java
+
+M:\mca\DS>java DoubleLinkedList
+MENU
+
+         1.Add Node At Front
+         2.Add Node At Rear
+         3.Remove Node At Front
+         4.Remove At Last
+         5.Add node At Position
+         6.Remove Node At Any Pos
+                 7.Display Elements in list
+enter your choice
+1
+enter data part to a node
+2
+
+
+ Do you want to continue...
+ press y or Y for(Yes)||n or N for(No)
+y
+MENU
+
+         1.Add Node At Front
+         2.Add Node At Rear
+         3.Remove Node At Front
+         4.Remove At Last
+         5.Add node At Position
+         6.Remove Node At Any Pos
+                 7.Display Elements in list
+enter your choice
+2
+Enter data part to a node
+5
+
+
+ Do you want to continue...
+ press y or Y for(Yes)||n or N for(No)
+y
+MENU
+
+         1.Add Node At Front
+         2.Add Node At Rear
+         3.Remove Node At Front
+         4.Remove At Last
+         5.Add node At Position
+         6.Remove Node At Any Pos
+                 7.Display Elements in list
+enter your choice
+4
+
+ Last node is deleted
+
+
+ Do you want to continue...
+ press y or Y for(Yes)||n or N for(No)
+y
+MENU
+
+         1.Add Node At Front
+         2.Add Node At Rear
+         3.Remove Node At Front
+         4.Remove At Last
+         5.Add node At Position
+         6.Remove Node At Any Pos
+                 7.Display Elements in list
+enter your choice
+7
+2---->
+
+ Do you want to continue...
+ press y or Y for(Yes)||n or N for(No)
+n
+
+
+ Thank you see  you again..
+
+
+
+M:\mca\DS>javac Queue1.java
+
+M:\mca\DS>java Queue1
+enter the queue size
+5
+
+         1.Enqueue
+         2.Dequeue
+         3.display
+         4.exit
+enter your choice
+1
+enter data ele in enqueue
+2
+
+
+ do you want to continue.../n press y or Y for(yes) ||n or N for(No)
+y
+
+         1.Enqueue
+         2.Dequeue
+         3.display
+         4.exit
+enter your choice
+2
+
+
+ do you want to continue.../n press y or Y for(yes) ||n or N for(No)
+y
+
+         1.Enqueue
+         2.Dequeue
+         3.display
+         4.exit
+enter your choice
+1
+enter data ele in enqueue
+8
+
+
+ do you want to continue.../n press y or Y for(yes) ||n or N for(No)
+y
+
+         1.Enqueue
+         2.Dequeue
+         3.display
+         4.exit
+enter your choice
+2
+
+
+ do you want to continue.../n press y or Y for(yes) ||n or N for(No)
+y
+
+         1.Enqueue
+         2.Dequeue
+         3.display
+         4.exit
+enter your choice
+1
+enter data ele in enqueue
+2
+
+
+ do you want to continue.../n press y or Y for(yes) ||n or N for(No)
+y
+
+         1.Enqueue
+         2.Dequeue
+         3.display
+         4.exit
+enter your choice
+2
+
+
+ do you want to continue.../n press y or Y for(yes) ||n or N for(No)
+3
+
+         1.Enqueue
+         2.Dequeue
+         3.display
+         4.exit
+enter your choice
+3
+queue is empty
+
+
+ do you want to continue.../n press y or Y for(yes) ||n or N for(No)
+y
+
+         1.Enqueue
+         2.Dequeue
+         3.display
+         4.exit
+enter your choice
+4
+
+M:\mca\DS>javac Main.java
+
+M:\mca\DS>java Main
+enter the enqueue elements
+3
+4
+5
+7
+8
+9
+DequeuedValue:3
+4<-5<-7<-8<-9<-rear
+
+M:\mca\DS>javac BinarySearchTreeDeletion.java
+
+M:\mca\DS>java BinarySearchTreeDeletion
+
+         1. In - Order Traversal
+         2. Pre - Order Traversal
+         3. Post - Order Traversal
+Enter your choice::
+1
+In-Order Traversal
+1
+10
+25
+30
+50
+53
+55
+60
+75
+76
+80
+85
+100
+
+Do you want to delete a node enter a node value else press 0
+1
+Node1is deleted please select any traversal technique to see the result
+do you want to continue...
+ Press Y||y for 'Yes' OR N || n for 'No'
+y
+
+         1. In - Order Traversal
+         2. Pre - Order Traversal
+         3. Post - Order Traversal
+Enter your choice::
+2
+Pre-Order Traversal
+50
+25
+10
+30
+75
+60
+55
+53
+85
+80
+76
+100
+Do you want to delete a node enter a node value else press 0
+3
+Node3is deleted please select any traversal technique to see the result
+do you want to continue...
+ Press Y||y for 'Yes' OR N || n for 'No'
+y
+
+         1. In - Order Traversal
+         2. Pre - Order Traversal
+         3. Post - Order Traversal
+Enter your choice::
+3
+post-Order Traversal
+10
+30
+25
+53
+55
+60
+76
+80
+100
+85
+75
+50
+Do you want to delete a node enter a node value else press 0
+4
+Node4is deleted please select any traversal technique to see the result
+do you want to continue...
+ Press Y||y for 'Yes' OR N || n for 'No'
+n
+Thank you and see you again.....
+
+M:\mca\DS>javac connectedcomponent.java
+
+M:\mca\DS>java connectedcomponent
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+A       1
+B       2
+C       3
+D       4
+E       5
+F       6
+G       7
+H       8
+I       9
+J       10
+K       11
+L       12
+
+M:\mca\DS>javac AirLineReservationSystem.java
+
+M:\mca\DS>java AirLineReservationSystem
+1.Reserve a seat
+2.view all seats
+3.exit
+1
+Enter seat number:9
+seat reserved.Thank you
+1.Reserve a seat
+2.view all seats
+3.exit
+2
+Seat1:Available
+Seat2:Available
+Seat3:Available
+Seat4:Available
+Seat5:Available
+Seat6:Available
+Seat7:Available
+Seat8:Available
+Seat9:Reserved
+Seat10:Available
+1.Reserve a seat
+2.view all seats
+3.exit
+3
+
